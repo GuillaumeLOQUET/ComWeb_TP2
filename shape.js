d5cfe3b4b30869f1fdf263db3a3ef50f8d8e6045
Guillaume LOQUET <guillaume.loquet@isen-ouest.yncrea.fr>
2020-05-27 16:59:45 +0200

    Initial commit

---
+++ shape.js
@@ -0,0 +1,10 @@
+function Shape(x, y){
+    this.center = new Point(x, y);
+}
+Shape.prototype.setCenter = function(point){
+    this.center = point;
+}
+Shape.prototype.draw = function(){};
+Shape.prototype.getCenter = function(point){
+    return this.center;
+}
